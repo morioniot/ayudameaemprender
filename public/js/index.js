6339bbd30fa000caec3963ab62672112995d3a18
Miguel Ramos <miguel9ramos@gmail.com>
2017-02-13 18:13:32 -0500

    Optimization in navigation bar position control

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,50 @@
-var mainImage = document.getElementById('main_image');
-var navigation = document.getElementById('navigation_bar');
+var NavigationBarControlFactory = function() {
+
+    var navFixedPositionState = false;
+    var body = document.body || document.documentElement;
+
+    var navigationElements = {
+        navigationBar: document.getElementById('navigation_bar')
+    }
+
+    var sectionHeights = {
+        mainImageHeight: document.getElementById('main_image').clientHeight,
+        skillsSectionHeight: document.getElementById('skills_section').clientHeight,
+        projectsSectionHeight: document.getElementById('projects_section').clientHeight
+    };
+
+    var navItemsState = {
+        skills: false,
+        projects: false,
+        contact: false
+    };
+
+    var changePosition = function() {
+        if(body.scrollTop >= sectionHeights.mainImageHeight - 80) {
+            if( !navFixedPositionState ) {
+                navFixedPositionState = true;
+                navigationElements.navigationBar.style.position = 'fixed';
+                navigationElements.navigationBar.style.top = '80px';
+            }
+        }
+        else {
+            if( navFixedPositionState ) {
+                navFixedPositionState = false;
+                navigationElements.navigationBar.style.position = 'absolute';
+                navigationElements.navigationBar.style.top = '100%';
+            }
+        }
+    };
+
+    return {
+        changePosition: changePosition
+    };
+}
+
 var body = document.body || document.documentElement;
-var mainImageVerticalSize = mainImage.clientHeight;
+var navigationManager = NavigationBarControlFactory();
 
-var changePosition = function() {
-    console.log(mainImageVerticalSize);
-    if(body.scrollTop >= mainImageVerticalSize - 80) {
-        navigation.style.position = 'fixed';
-        navigation.style.top = '80px';
-    }
-    else {
-        navigation.style.position = 'absolute';
-        navigation.style.top = '100%';
-    }
+body.onscroll = function(){
+    navigationManager.changePosition();
 };
-
-body.onscroll = changePosition;
 console.log("Entrando a la consola");
